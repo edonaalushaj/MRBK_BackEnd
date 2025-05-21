111ce618255d0ff54c796b00215dd7e6e39b21a9
EldaDrenica <123164394+EldaDrenica@users.noreply.github.com>
2025-05-21 11:01:27 +0200

    Delete src/main/com.example.backend directory

--- src/main/com.example.backend/repository/ReservationFoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.ReservationFood;
-import com.example.backend.model.ReservationFoodId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationFoodRepository extends JpaRepository<ReservationFood, ReservationFoodId> {
-}
